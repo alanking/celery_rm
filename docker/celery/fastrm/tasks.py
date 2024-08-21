671ce04a0b613453cf8daa0ba754801f022b750f
Alan King <alanking@renci.org>
2024-08-21 14:28:50 -0400

    Initial remove collection task
    This commit adds an initial implementation for a recursive collection
    removal Celery task. Sub-collections are scheduled as their own collection
    removal tasks and data objects are scheduled as individual removals.
    
    Many optimizations can be had here.

--- docker/celery/fastrm/tasks.py
+++ docker/celery/fastrm/tasks.py
@@ -3,7 +3,7 @@ from .celery import app
 import os
 
 from irods.column import Criterion
-from irods.exception import CollectionDoesNotExist
+from irods.exception import CollectionDoesNotExist, DataObjectDoesNotExist
 from irods.models import DataObject, DataObjectMeta, Collection, CollectionMeta
 from irods.session import iRODSSession
 
@@ -34,3 +34,28 @@ def list_collection(logical_path, recursive=False):
 		"data_objects": [do.path for do in data_objects],
 		"subcollections": [sc.path for sc in subcollections]
 	}
+
+@app.task
+def remove_collection(logical_path):
+	"""Remove all data objects in a collection and its subcollections."""
+	with iRODSSession(**delete_this_env) as session:
+		try:
+			target_collection = session.collections.get(logical_path)
+		except CollectionDoesNotExist:
+			# Print an error message here because the exception doesn't tell you what doesn't exist.
+			print(f"Collection [{logical_path}] does not exist.")
+			raise
+		for data_object in target_collection.data_objects:
+			remove_data_object.delay(data_object.path)
+		for subcollection in target_collection.subcollections:
+			remove_collection.delay(subcollection.path)
+
+@app.task
+def remove_data_object(logical_path, no_trash=False):
+	"""Remove a data object, optionally omitting rename to trash collection."""
+	with iRODSSession(**delete_this_env) as session:
+		try:
+			session.data_objects.unlink(logical_path, force=not no_trash)
+		except DataObjectDoesNotExist:
+			print(f"Data object [{logical_path}] does not exist.")
+			raise
